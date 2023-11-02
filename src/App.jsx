680673f58cfe312a72cdeb0213b3537c6455f173
unknown <you@example.com>
2023-11-02 17:03:46 -0600

    configuracion inicial frontend con React Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <>
+    <div className="bg-transparent">
+      <h1 className="text-white text-center text-5xl font-black mt-5">Control de Vehículos</h1>
+    </div>
+  </>;
+}
+
+export default App;
