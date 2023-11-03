6940e73fb6591de21f16384569580aa17e50e8a6
unknown <you@example.com>
2023-11-03 17:00:18 -0600

    Pagina principal en principio terminada

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,26 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>
-    <div className="bg-transparent">
-      <h1 className="text-white text-center text-5xl font-black mt-5">Control de Vehículos</h1>
-    </div>
-  </>;
+  return (
+    <>
+      <div className="bg-transparent grid grid-cols-3 gap-3 text-center">
+        <button className=" boton1 text-white text-2xl font-semibold">
+          Registrar Vehículo
+        </button>
+        <h1 className="text-white text-5xl font-black mt-5">
+          Control de Vehículos
+        </h1>
+        <button className="boton2 text-white text-2xl font-semibold">
+          Registrar Entradas/Salidas
+        </button>
+      </div>
+      <div className="contenedor-carro grid grid-cols-3 gap-3">
+        <img className="imgCarro" src="img/silverado.png" alt="" />
+        <img className="imgCarro" src="img/camaro.png" alt="" />
+        <img className="imgCarro" src="img/escape.png" alt="" />
+      </div>
+    </>
+  );
 }
 
 export default App;
